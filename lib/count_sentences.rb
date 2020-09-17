f86f2b278c476d2e623012de721253d628a35ae7
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-09-17 16:51:35 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    array = self.split(/[.?!]/).reject {|x| x.empty?}
+    array.length 
   end
 end
